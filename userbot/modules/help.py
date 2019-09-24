efeab109e2cce1b401a201c0a7e2fd9eef9f8a9e
Ryan AF <ryan19adi@gmail.com>
2019-09-24 23:50:37 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,5 +25,5 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\n"
+            string += "`||"
         await event.reply(string)
