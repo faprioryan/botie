0480d043e0275907a3582ac340de13d7aaaecdb3
Ryan AF <ryan19adi@gmail.com>
2019-11-18 12:03:52 +0700

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -864,44 +864,86 @@ async def stretch(stret):
     await stret.edit(reply_text)
 
 
+#@register(outgoing=True, pattern="^.zal(?: |$)(.*)")
+#@errors_handler
+#async def zal(zgfy):
+#    """ Invoke the feeling of chaos. """
+#    reply_text = list()
+#    textx = await zgfy.get_reply_message()
+#    message = zgfy.pattern_match.group(1)
+#    if message:
+#        pass
+#    elif textx:
+#        message = textx.text
+#    else:
+#        await zgfy.edit(
+#            "`gͫ ̆ i̛ ̺ v͇̆ ȅͅ   a̢ͦ   s̴̪ c̸̢ ä̸ rͩͣ y͖͞   t̨͚ é̠ x̢͖  t͔͛`"
+#        )
+#        return
+#
+#    for charac in message:
+#        if not charac.isalpha():
+#            reply_text.append(charac)
+#            continue
+#
+#        for _ in range(0, 3):
+#            randint = random.randint(0, 2)
+#
+#            if randint == 0:
+#                charac = charac.strip() + \
+#                    random.choice(ZALG_LIST[0]).strip()
+#            elif randint == 1:
+#                charac = charac.strip() + \
+#                    random.choice(ZALG_LIST[1]).strip()
+#            else:
+#                charac = charac.strip() + \
+#                    random.choice(ZALG_LIST[2]).strip()
+#
+#        reply_text.append(charac)
+#
+#    await zgfy.edit("".join(reply_text))
+
+                      
+
 @register(outgoing=True, pattern="^.zal(?: |$)(.*)")
-@errors_handler
 async def zal(zgfy):
     """ Invoke the feeling of chaos. """
-    reply_text = list()
-    textx = await zgfy.get_reply_message()
-    message = zgfy.pattern_match.group(1)
-    if message:
-        pass
-    elif textx:
-        message = textx.text
-    else:
-        await zgfy.edit(
-            "`gͫ ̆ i̛ ̺ v͇̆ ȅͅ   a̢ͦ   s̴̪ c̸̢ ä̸ rͩͣ y͖͞   t̨͚ é̠ x̢͖  t͔͛`"
-        )
-        return
+    if not zgfy.text[0].isalpha() and zgfy.text[0] not in ("/", "#", "@", "!"):
+        reply_text = list()
+        textx = await zgfy.get_reply_message()
+        message = zgfy.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await zgfy.edit(
+                "`gͫ ̆ i̛ ̺ v͇̆ ȅͅ   a̢ͦ   s̴̪ c̸̢ ä̸ rͩͣ y͖͞   t̨͚ é̠ x̢͖  t͔͛`"
+            )
+            return
 
-    for charac in message:
-        if not charac.isalpha():
+        for charac in message:
+            if not charac.isalpha():
+                reply_text.append(charac)
+                continue
+
+            for _ in range(0, 3):
+                randint = random.randint(0, 2)
+
+                if randint == 0:
+                    charac = charac.strip() + \
+                        random.choice(ZALG_LIST[0]).strip()
+                elif randint == 1:
+                    charac = charac.strip() + \
+                        random.choice(ZALG_LIST[1]).strip()
+                else:
+                    charac = charac.strip() + \
+                        random.choice(ZALG_LIST[2]).strip()
+
             reply_text.append(charac)
-            continue
 
-        for _ in range(0, 3):
-            randint = random.randint(0, 2)
+        await zgfy.edit("".join(reply_text))
 
-            if randint == 0:
-                charac = charac.strip() + \
-                    random.choice(ZALG_LIST[0]).strip()
-            elif randint == 1:
-                charac = charac.strip() + \
-                    random.choice(ZALG_LIST[1]).strip()
-            else:
-                charac = charac.strip() + \
-                    random.choice(ZALG_LIST[2]).strip()
-
-        reply_text.append(charac)
-
-    await zgfy.edit("".join(reply_text))
 
 
 @register(outgoing=True, pattern="^.hi$")
